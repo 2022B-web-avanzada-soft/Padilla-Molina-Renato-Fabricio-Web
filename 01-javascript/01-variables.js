e8cd8c7f10b69c611d4ec0929ca45042ca8d5779
Renato Padilla <renato.padilla@epn.edu.ec>
2022-11-14 17:58:14 -0500

    Hasta variables

---
+++ 01-javascript/01-variables.js
@@ -0,0 +1,27 @@
+// variables mutables
+var numeroA = 1;
+let numeroB = 2;
+numeroA = 12;
+numeroB = 8;
+numeroA = false;
+numeroB = true;
+
+// variables inmutables
+const extensionPdf = 'PDF';
+
+// Tipos de variables
+const numero = 1;
+const sueldo = 1.2;
+const texto = 'Renato';
+const apellidos = "Padilla";
+const booleano = false;
+const hijos = null;
+const zapatos = undefined;
+
+console.log(typeof numero);
+console.log(typeof sueldo);
+console.log(typeof texto);
+console.log(typeof apellidos);
+console.log(typeof booleano);
+console.log(typeof hijos);
+console.log(typeof zapatos);
