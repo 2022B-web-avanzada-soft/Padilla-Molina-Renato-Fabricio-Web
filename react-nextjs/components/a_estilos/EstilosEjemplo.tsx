c302ca11f0d8d7e938a5dd50c421806d4bff0649
Renato Padilla <renato.padilla@epn.edu.ec>
2023-01-03 19:07:26 -0500

    React componentes

---
+++ react-nextjs/components/a_estilos/EstilosEjemplo.tsx
@@ -0,0 +1,14 @@
+import styles from './estilos.module.css'
+export default function () {
+    const misEstilos = {
+        color: "white",
+        backgroundColor: "black",
+        borderBottom: "5px solid yellow"
+    };
+        return (
+        <>
+            <div style={misEstilos}> Otros Estilos </div>
+            <><div className={styles.rojo}>Hola</div></>
+        </>
+    )
+}
---
+++ react-nextjs/pages/a_hola_mundo.tsx
@@ -0,0 +1,23 @@
+/*
+const a_componente = function() {
+    return (
+        <></>
+    )
+}
+export default a_componente
+const b_componente = () =>{
+    return (
+        <></>
+    )
+}
+export default b_componente*/
+import EstilosEjemplo from "../components/a_estilos/EstilosEjemplo";
+
+export default function a_hola_mundo() {
+    return (
+        <>
+            <h1>Hola Mundo</h1>
+            <EstilosEjemplo></EstilosEjemplo>
+        </>
+    )
+}
